91b34000af2f90f2b5fb950f727b03b50ceb63ba
Alex Kopylov <akopylov@sfu.ca>
2022-06-07 01:39:56 -0700

    connected to database

--- index.js
+++ index.js
@@ -3,10 +3,34 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const { Pool } = require('pg');
+var pool;
+pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+})
+
+var app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => res.render('pages/index'))
+
+
+app.get('/database', (req, res)=> {
+  var getUsersQuery = 'SELECT * FROM us2er';
+  pool.query(getUsersQuery, (error, result)=>{
+    if(error)
+      res.end(error);
+    var results = {'rows':result.rows}
+    res.render('pages/db', results)
+  })
+  
+})
+
+
+app.get('/cool', (req, res) => res.send(cool()))
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
