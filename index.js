fb8895a230ce5302375830e33ff7bb3bd92deffa
Alex Kopylov <akopylov@sfu.ca>
2022-06-07 02:02:14 -0700

    commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => res.render('pages/index'))
+app.get('/', (req, res) => {res.render('pages/index')})
 
 
 app.get('/database', (req, res)=> {
