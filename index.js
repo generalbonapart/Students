b05c1020aa2385804b875c1ef9a89b2b67a89940
Alex Kopylov <akopylov@sfu.ca>
2022-06-14 11:53:52 -0700

    done raw ass2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 console.log(process.env.DATABASE_URL_1)
 
-
+student_id = 4
 
 const { Pool } = require('pg');
 var pool;
@@ -23,16 +23,82 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {res.render('pages/index')})
 
 
-app.get('/database', (req, res)=> {
-  var getUsersQuery = 'SELECT * FROM student';
+app.get('/class', (req, res)=> {
+  var getUsersQuery = 'SELECT * FROM student order by id;';
   pool.query(getUsersQuery, (error, result)=>{
     if(error)
       res.end(error);
     var results = {'rows':result.rows}
 
-    res.render('pages/db', results)
+    res.render('pages/display', results)
   })
 })
 
+app.get('/student/:id', (req, res)=> {
+  var id = req.params.id;
+  var getStudentQuery = `SELECT * FROM student where id=${id};`;
+  pool.query(getStudentQuery, (error, result)=>{
+    if(error)
+      res.end(error);
+    var results = {'student': result.rows};
+    res.render('pages/update', results);
+  
+  })
+})
+
+app.post('/adduser', (req, res) => {
+  console.log("add request")
+  var name = req.body.name
+  var height = req.body.height
+  var weight = req.body.weight
+  var gpa = req.body.gpa
+  var hair = req.body.hair
+  var addStudentQuery = `insert into student (name,height,weight,hair_color, gpa) values  ('${name}','${height}', '${weight}','${hair}','${gpa}')`;
+  pool.query(addStudentQuery, (error, result)=>{
+    if(error)
+      res.send(error);
+    else
+      console.log(student_id);
+      student_id += 1;
+      res.redirect('/class')
+    })
+ 
+})
+
+app.post('/update', (req, res) => {
+  console.log("update request")
+  var id = req.body.id
+  var name = req.body.name
+  var height = req.body.height
+  var weight = req.body.weight
+  var gpa = req.body.gpa
+  var hair = req.body.hair
+  var updateStudentQuery = `update student set name ='${name}', height = '${height}', weight = '${weight}',hair_color = '${hair}', gpa = '${gpa}' where id='${id}';`;
+  pool.query(updateStudentQuery, (error, result)=>{
+    if(error)
+      res.send(error);
+    else
+      console.log(student_id);
+      res.redirect('/class')
+    })
+ 
+})
+
+app.post('/delete', (req, res) => {
+  console.log("delete request")
+  var id = req.body.id;
+  var deleteStudentQuery = `delete from student where id='${id}';`;
+  pool.query(deleteStudentQuery, (error, result)=>{
+    if(error)
+      res.send(error);
+    else
+      console.log(id);
+      res.redirect('/class')
+    })
+ 
+})
+
+
 app.get('/cool', (req, res) => res.send(cool()))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
